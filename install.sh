298f4753cc73cc4b2920042d2781d3e7db6bb009
theandrunique <theandrunique@gmail.com>
2024-03-10 15:55:42 +0300

    Update instal.sh file

--- install.sh
+++ install.sh
@@ -1,25 +1,31 @@
-
+sudo apt update
+sudo apt install gcc -y
+sudo apt install npm -y
 # installing zsh
 sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # installing tmux
-sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt install tmux -y
 
 # installing neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
 
 sudo apt install fzf -y
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-export PATH="$PATH:/opt/nvim-linux64/bin"
-
+# others
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # installing stow
 
 sudo apt install stow -y
-stow .
 
+rm ~/.zshrc
+rm -rf ~/.config/
+
+stow .
+rm ~/install.sh
+
+tmux source-file ~/.tmux.conf
